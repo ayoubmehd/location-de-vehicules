f598fa59eb1a3ddbcee1eba716cf132c01d26ed0
ayoubmehd <safi-_-gio-raja@live.fr>
2021-02-18 10:36:49 +0100

    fix contact form validation bug

--- js/validate.js
+++ js/validate.js
@@ -3,30 +3,34 @@ var modalAlert = document.querySelector(".alert");
 modalAlert.addEventListener("click", function () {
   modalAlert.style.display = "none";
 });
-var error = false;
 
 Array.from(contactForm).forEach((element) => {
   element.addEventListener("focus", hideError);
 });
 
 function nullValidation() {
+  var error = false;
   Array.from(contactForm).forEach((element) => {
     if (
-      !element.nextElementSibling &&
       !element.value &&
       (element.tagName === "INPUT" || element.tagName === "TEXTAREA")
     ) {
-      element.insertAdjacentHTML(
-        "afterend",
-        `
+      console.log(element.value);
+      if (!element.nextElementSibling) {
+        element.insertAdjacentHTML(
+          "afterend",
+          `
             <div class="error-msg required my t-danger">
                 This field is required
             </div>
           `
-      );
+        );
+      }
       error = true;
     }
   });
+
+  return error;
 }
 
 function hideError(event) {
@@ -37,72 +41,64 @@ function hideError(event) {
 
 function nameValidation() {
   var regexp = /[a-zA-Z]+/g;
+  var error = false;
 
-  if (
-    !regexp.test(contactForm["name"].value) &&
-    !contactForm.name.nextElementSibling
-  ) {
-    contactForm.name.insertAdjacentHTML(
-      "afterend",
-      `
+  if (!regexp.test(contactForm["name"].value)) {
+    if (!contactForm.name.nextElementSibling) {
+      contactForm.name.insertAdjacentHTML(
+        "afterend",
+        `
             <div class="error-msg not-valid my t-danger">
                 Name should contain only charachter
             </div>
       `
-    );
+      );
+    }
 
     error = true;
-
-    return;
   }
-
-  error = false;
+  return error;
 }
 
 function emailValidation() {
   var regExp = /^[\w-]+@([\w-]+\.)+[\w-]+/;
-
-  console.log(regExp.test(contactForm["email"].value));
-  if (
-    !regExp.test(contactForm["email"].value) &&
-    !contactForm.email.nextElementSibling
-  ) {
-    contactForm.email.insertAdjacentHTML(
-      "afterend",
-      `
+  var error = false;
+  if (!regExp.test(contactForm["email"].value)) {
+    if (!contactForm.email.nextElementSibling) {
+      contactForm.email.insertAdjacentHTML(
+        "afterend",
+        `
             <div class="error-msg not-valid my t-danger">
                 This is not a valid email
             </div>
       `
-    );
+      );
+    }
 
     error = true;
-    return;
   }
 
-  error = false;
+  return error;
 }
 
 // function subjectValidation() {}
 
 function contentValidation() {
-  if (
-    contactForm["content"].value.length < 60 &&
-    !contactForm.content.nextElementSibling
-  ) {
-    contactForm.content.insertAdjacentHTML(
-      "afterend",
-      `
+  var error = false;
+  if (contactForm["content"].value.length < 60) {
+    if (!contactForm.content.nextElementSibling) {
+      contactForm.content.insertAdjacentHTML(
+        "afterend",
+        `
             <div class="error-msg not-valid my t-danger">
                 Content is too short
             </div>
       `
-    );
+      );
+    }
     error = true;
-    return;
   }
-
-  error = false;
+  return error;
 }
 
 function sendForm() {
@@ -121,12 +117,14 @@ function sendForm() {
 
 contactForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  nullValidation();
-  nameValidation();
-  emailValidation();
-  contentValidation();
-
-  if (error) return;
+  if (nullValidation()) return;
+  // console.log("null", nullValidation());
+  if (nameValidation()) return;
+  // console.log("name", nameValidation());
+  if (emailValidation()) return;
+  // console.log("email", emailValidation());
+  if (contentValidation()) return;
+  // console.log("content", contentValidation());
 
   sendForm();
 });
